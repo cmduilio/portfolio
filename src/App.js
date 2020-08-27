9855abb2566062b29a8b0882388e64a1930b443d
Mauro Candotti <cmduilio@gmail.com>
2020-08-27 11:24:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+   return (
+      <Router>
+        <>
+          <Route exact path='/' component={Home}/>
+        </>
+      </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from 'react';
+
+function Home() {
+    return (
+        <section>
+            {/*<Navbar />*/}
+            <div className="container">
+                <div className="row">
+                    <div className="col s12 m4 l3">
+                        {/*<Profile />*/}
+                        asd
+                    </div>
+                    <div className="col s12 m8 l9">
+                        {/*<About />*/}
+                        dsa
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
