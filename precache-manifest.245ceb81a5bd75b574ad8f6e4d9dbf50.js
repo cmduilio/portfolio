d7cd6e88e2e8384a593db1411374918ce1aa6d69
Mauro Candotti <cmduilio@gmail.com>
2020-08-29 18:11:03 -0300

    Updates

---
+++ precache-manifest.245ceb81a5bd75b574ad8f6e4d9dbf50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7a61d2d892a02798694e5566b38c19",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "63c30c6e5f25675c0570",
+    "url": "/portfolio/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "eff33575ff8a6141fee1",
+    "url": "/portfolio/static/css/main.d2d6db25.chunk.css"
+  },
+  {
+    "revision": "63c30c6e5f25675c0570",
+    "url": "/portfolio/static/js/2.8af02531.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.8af02531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff33575ff8a6141fee1",
+    "url": "/portfolio/static/js/main.beb08a7c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
