ea33de15fe187ab7917eb32a1612d0b68472a23b
Mauro Candotti <cmduilio@gmail.com>
2020-08-31 14:39:12 -0300

    Updates

---
+++ precache-manifest.a5454c6a605123d63bbcda7a703b0086.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e13f1aca337467e74e33688acac16cd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cafa7eb3b10dc80921db",
+    "url": "/portfolio/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "231224a38169c2fb0d40",
+    "url": "/portfolio/static/css/main.d2d6db25.chunk.css"
+  },
+  {
+    "revision": "cafa7eb3b10dc80921db",
+    "url": "/portfolio/static/js/2.a3f0d488.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.a3f0d488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231224a38169c2fb0d40",
+    "url": "/portfolio/static/js/main.95655d8a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1d6132400c0f507985f19f7d771f8104",
+    "url": "/portfolio/static/media/Error.1d613240.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a8d52aaa3b69b5a389e2a68ed225efe2.js"
+  "/portfolio/precache-manifest.a5454c6a605123d63bbcda7a703b0086.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
